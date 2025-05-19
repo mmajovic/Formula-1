2c17f3109c0ada137f4f5fb93c505a6e147d2538
Ljubica Aleksic <ljubica_aleksic@yahoo.com>
2025-05-19 15:05:25 +0200

    Races format code

--- src/components/Races.jsx
+++ src/components/Races.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
-import { useNavigate} from "react-router"
+import { useNavigate } from "react-router"
 import Loader from "./Loader";
 import axios from "axios";
 
-export default function Races () {
+export default function Races() {
    const [races, setRaces] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    
+   const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+
+   useEffect(() => {
       getRaces();
-  }, []);
+   }, []);
 
-  const getRaces = async () => {
+   const getRaces = async () => {
       const url = "http://ergast.com/api/f1/2013/results/1.json";
       const response = await axios.get(url);
       console.log(response);
@@ -20,14 +20,14 @@ export default function Races () {
       console.log(response.data.MRData.RaceTable.Races);
       setRaces(response.data.MRData.RaceTable.Races);
       setIsLoading(false);
-  };
+   };
 
-  if (isLoading) {
-   return (<Loader />)
-}
+   if (isLoading) {
+      return (<Loader />)
+   }
 
-     return (
-        <div>
+   return (
+      <div>
          <h1>Race Calendar</h1>
          <table>
             <tbody>
@@ -42,19 +42,19 @@ export default function Races () {
                   <th>Winner</th>
                </tr>
                {races.map((race) => {
-                        return (
-                            <tr key={race.round}>
-                                <td>{race.round}</td>
-                                <td>{race.raceName}</td>
-                                <td>{race.Circuit.circuitName}</td>
-                                <td>{race.date}</td>
-                                <td>{race.Results[0].Driver.familyName}</td>
-                            </tr>
-                        )
-                    })}
+                  return (
+                     <tr key={race.round}>
+                        <td>{race.round}</td>
+                        <td>{race.raceName}</td>
+                        <td>{race.Circuit.circuitName}</td>
+                        <td>{race.date}</td>
+                        <td>{race.Results[0].Driver.familyName}</td>
+                     </tr>
+                  )
+               })}
             </tbody>
          </table>
-        </div>
-     );
+      </div>
+   );
 
 }
